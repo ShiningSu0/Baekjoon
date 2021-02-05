12d6815b376511d4a63a3a94cb672aa2e1f6c077
Suyeong Kim 김수영 <44190559+ShiningSu0@users.noreply.github.com>
2021-02-05 12:13:15 +0900

    Update 16508.py

--- 16508.py
+++ 16508.py
@@ -1,43 +1,82 @@
 #N,K=map(int, input().split())
 #a1=list(map(int, input().split()))
-from itertools import combinations
-import copy
-T=list(input())
-N=int(input())
-target=[0]*26
-words=[]
+N,M=map(int,input().split())
+ary=[]
 for i in range(N):
-    C,W=map(str,input().split())
-    temp=[0]*26
-    for j in list(W):
-        temp[ord(j)-65]+=1
-    words.append([int(C),temp])
-for i in T:
-    target[ord(i)-65]+=1
-#찾으려는 단어 빈도수 배열 target[26]
-#words-> 전공책도 각각 크기 26의 빈도수 배열로 변환
+    a=list(input())
+    for j in range(len(a)):
+        a[j]=int(a[j])
+    ary.append(a)
+visited=[[0]*M for i in range(N)]
+visited[0][0]=1
+wall=[[0]*M for i in range(N)]
+'''
+block=[]
+dc = [1, -1, 0, 0]
+dr = [0, 0, -1, 1]
+for i in range(N):
+    for j in range(M):
+        cnt=0
+        for k in range(4):
+            r=i+dr[k]
+            c=j+dc[k]
+            if 0<=c<M and 0<=r<N and ary[r][c]==0 and ary[i][j]==1:
+                cnt+=1
+        if cnt!=0:
+            block.append([i,j])
+'''
+def bfs(start_row, start_col,visited):
+
+    dc = [1, -1, 0, 0]
+    dr = [0, 0, -1, 1]
+    queue = [[start_row, start_col,True]]
+    while queue:
+        row,col = queue[0][0], queue[0][1]
+        block=queue[0][2]
+        queue.pop(0)        
+        for i in range(4):
+            r = row + dr[i]
+            c = col + dc[i]
+            if 0 <= c < M and 0 <= r < N:
+                if ary[r][c] == 0 and visited[r][c]==0 and block:
+                    visited[r][c]=1+visited[row][col]
+                    queue.append([r, c,block])
+                elif ary[r][c] == 0 and wall[r][c]==0 and not block:
+                    wall[r][c]=1+wall[row][col]
+                    queue.append([r, c,block])
+                if block and ary[r][c]==1 and wall[r][c]==0:
+                    wall[r][c]=1+visited[row][col]
+                    queue.append([r,c,0])
+                
 
 ans=[]
-for i in range(1,N+1):
-    for lst in list(combinations(words,i)):#개수 별 모든 전공책 조합 생성
-        temp=copy.deepcopy(target)
-        price=0#가격 초기화
-        for case in lst: # 조합에서 전공책 빈도수배열 하나씩 빼서 비교
-            price+=case[0]
-            for j in range(26): 
-                if temp[j]>0 and case[1][j]>0:#찾으려는 단어 글자 빈도수 >0이고 전공책 빈도수배열 글자 빈도수 >0이면
-                    temp[j]-=case[1][j]# 서로 빼버림
-        cnt=0
-        for k in range(26):
-            if temp[k]>0:#다 찾으면 target 배열 체크해서
-                continue
-            else:
-                cnt+=1
-        if cnt==26:#다 찾아썼으면 가격을 업데이트
-            ans.append(price)
-if len(ans)==0:
+bfs(0,0,visited)
+print(visited)
+print(wall)
+if visited[-1][-1]==0 and wall[-1][-1]==0:
     print(-1)
+elif visited[-1][-1]!=0 and wall[-1][-1]==0:
+    print(visited[-1][-1])
+elif visited[-1][-1]==0 and wall[-1][-1]!=0:
+    print(wall[-1][-1])
+elif visited[-1][-1]!=0 and wall[-1][-1]!=0:
+    print(min(visited[-1][-1],wall[-1][-1]))
+'''
+ans.append(visited[N-1][M-1])
+for i in range(len(block)):
+    visited=[[0]*M for i in range(N)]
+    visited[0][0]=1
+    ary[block[i][0]][block[i][1]]=0
+    bfs(0,0,visited)
+    ans.append(visited[N-1][M-1])
+
+
+real=[]
+for i in range(ans):
+    if ans[i]>0:
+        real.append(ans[i])
+if len(real)>0:
+    print(min(real))
 else:
-    print(min(ans))#가격 최소값
-
-
+    print(-1)
+'''
